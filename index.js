ba80e1ffddb4a2f1cf3ed254d8914c138b72afe5
zahidhr99 <zahidhr99@gmail.com>
2025-07-24 23:56:55 +0600

    update

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.get("/", (req, res) => {
 require("./routes/task.routes.js")(app);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,17 +1,17 @@
 module.exports = app => {
-    const tasks = require("../controllers/task.controller.js");
+  const tasks = require("../controllers/task.controller.js");
   
-    var router = require("express").Router();
+  var router = require("express").Router();
   
-    router.post("/", tasks.create);
+  router.post("/", tasks.create);
   
-    router.get("/", tasks.findAll);
+  router.get("/", tasks.findAll);
   
-    router.get("/:taskId", tasks.findOne);
+  router.get("/:taskId", tasks.findOne);
   
-    router.put("/:taskId", tasks.update);
+  router.put("/:taskId", tasks.update);
   
-    router.delete("/:taskId", tasks.delete);
+  router.delete("/:taskId", tasks.delete);
   
-    app.use('/api/tasks', router);
-  };
+  app.use('/api/tasks', router);
+};
